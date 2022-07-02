5b3ffeb17e85c0fa67d2073e48d079b8d0da870e
ilbounce <ilbounce98@yandex.ru>
2022-07-03 00:39:37 +0300

    ADD: pytransaq

---
+++ README.md
@@ -0,0 +1,12 @@
+# PyTransaq
+    The python wrapper of Transaq Connector from https://www.finam.ru/howtotrade/Welcome/#best.area_a
+    Transaq Connector documentation: https://www.finam.ru/files/TXmlConnector.pdf
+
+## Requirements
+    elementpath
+    socket
+
+## Test
+    ```
+    python test.py
+    ```
---
+++ Transaq/Transaq/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Transaq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ports.commandListenerPort = Int32.Parse(args[0]);
+            Ports.PingPort = Int32.Parse(args[1]);
+            Ports.newsListenerPort = Int32.Parse(args[2]);
+            Ports.subscribeDataListenerPort = Int32.Parse(args[3]);
+            Ports.dataListenerPort = Int32.Parse(args[4]);
+            Ports.accountListenerPort = Int32.Parse(args[5]);
+
+            Server server = new Server();
+            server.Run();
+        }
+    }
+}
